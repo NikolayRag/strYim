ca97840d9d72f93c5129a881dfb14191d44e4aa2
NikolayRag <nikolayr@mail.ru>
2016-10-29 05:16:15 +0300

    add: support collection

--- src/kiSupport.py
+++ src/kiSupport.py
@@ -21,3 +21,17 @@ class KiLog():
 	def set(self, _ok=True, _err=True):
 		self.logOk= _ok
 		self.logErr= _err
+
+
+
+
+
+
+def getA(_var, _field, _default=False):
+	if not isinstance(_var, dict):
+		return _default
+
+	if not _field in _var:
+		return _default
+
+	return _var[_field]
