93386b16d883a88157e3d05789250531fbd87ebe
NikolayRag <nikolayr@mail.ru>
2016-11-05 23:14:43 +0300

    add: mp4Restore prototype

--- src/stryim.py
+++ src/stryim.py
@@ -1,6 +1,33 @@
 import sublime, sublime_plugin
 import threading
 
+
+class mp4Restore():
+	cContext= None
+
+	def __init__(self):
+		None
+
+
+	'''
+	Provide raw mp4 data to parse.
+	Return numer of bytes actually consumed.
+
+		data
+			byte string mp4 data
+
+		context
+			arbitrary identifier of supplied data
+	'''
+	def parse(self, _data, _ctx):
+		if self.cContext!=_ctx:
+			None
+		print(len(_data), _ctx)
+		return min(len(_data), 10000000)
+
+
+
+
 from .byteTransit import *
 from .kiYiListener import *
 from .kiTelnet import *
@@ -20,11 +47,8 @@ class YiOnCommand(sublime_plugin.TextCommand):
 
 		KiTelnet.defaults('192.168.42.1', 'root', '', 8088)
 
-		def pp(data, ctx):
-			print(len(data), ctx)
-			return min(len(data), 10000000)
-
-		buffer= byteTransit(pp, 10000000)
+		restoreO= mp4Restore()
+		buffer= byteTransit(restoreO.parse, 10000000)
 		KiYi[0]= KiYiListener(buffer)
 		KiYi[0].start()
 		KiYi[0].live()
