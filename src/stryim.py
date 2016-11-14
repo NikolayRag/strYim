e6f63e6adfce983dd6beaa5ae587ce05108452cb
NikolayRag <nikolayr@mail.ru>
2016-11-14 16:37:34 +0300

    streamFFRTMP structurised

--- src/stryim.py
+++ src/stryim.py
@@ -2,25 +2,55 @@ import subprocess, threading, time, socket
 
 class streamFFRTMP():
 	tcp= None
+	tcpSock= None
+	h264Head= {
+		(1080,30,'low'): b'\x00\x00\x00\x01\'M@3\x9ad\x03\xc0\x11?,\x8c\x04\x04\x05\x00\x00\x03\x03\xe9\x00\x00\xea`\xe8`\x00\xb7\x18\x00\x02\xdcl\xbb\xcb\x8d\x0c\x00\x16\xe3\x00\x00[\x8d\x97ypxD"R\xc0\x00\x00\x00\x01(\xee8\x80'
+	}
 
-	def init(self):
-		threading.Timer(0, lambda: subprocess.call('D:/yi/restore/ff/ffmpeg -re -i tcp://localhost:2345 -vcodec copy -f flv rtmp://localhost:5130/live/yi/', shell=False)).start()
-
-		tcpSock= socket.socket()
-
-		tcpSock.bind(('127.0.0.1',2345))
-
-		tcpSock.listen(1)
-		self.tcp, a= tcpSock.accept()
+	initFlag= None
 
 
-	def go(self, aa):
+	def __init__(self):
+		self.tcp= None
 
-		self.tcp.sendall(aa)
+
+		self.initFlag= threading.Event()
+
+		ffport= 2345
+		threading.Timer(0, lambda: self.tcpInit(ffport)).start()
+		threading.Timer(0, lambda: self.serverInit(ffport)).start()
+
+		self.initFlag.wait();
+
+
+	def serverInit(self, _ffport):
+		subprocess.call('D:/yi/restore/ff/ffmpeg -re -i tcp://localhost:%d -vcodec copy -f flv rtmp://localhost:5130/live/yi/' % _ffport, shell=False)
+
+
+	def tcpInit(self, _ffport):
+		self.tcpSock= socket.socket()
+
+		self.tcpSock.bind(('127.0.0.1',_ffport))
+
+		self.tcpSock.listen(1)
+		self.tcp, a= self.tcpSock.accept()
+
+		self.tcp.sendall(self.h264Head[(1080,30,'low')])
+
+		self.initFlag.set()
+
+
+	def go(self, _atom, _data):
+		if self.tcp and _atom['type']=='H264':
+			self.tcp.sendall(b'\x00\x00\x00\x01' +_data[4:])
+
 
 	def stop(self):
-		self.tcp.close()
+		tcp= self.tcp
+		self.tcp= None
+		tcp.close()
 
+		self.tcpSock.close()
 
 
 
@@ -37,12 +67,14 @@ from .kiTelnet import *
 from .kiLog import *
 
 
-KiYi= [None]
+KiYi= [None, None]
 
 '''
 YiOn/Off commands are used to test Stryim in Sublime, `coz its lazy to set up running environment.
 '''
 class YiOnCommand(sublime_plugin.TextCommand):
+	
+
 	def cbConn(self, _mode):
 		kiLog.ok('Connected' if _mode else 'Disconnected')
 	def cbLive(self, _mode):
@@ -71,19 +103,9 @@ class YiOnCommand(sublime_plugin.TextCommand):
 		selfIP= KiTelnet.defaults(address='192.168.42.1')
 		
 
-		ff= streamFFRTMP()
-		ff.init()
-		fi= open('D:/yi/restore/stryim/h', 'rb')
-		ff.go(fi.read())
-		fi.close()
+		KiYi[1]= streamFFRTMP()
 
-
-		def abs(a, d):
-			if a['type']=='H264':
-				ff.go(b'\x00\x00\x00\x01')
-				ff.go(d[4:])
-
-		restoreO= mp4RecoverExe(abs)
+		restoreO= mp4RecoverExe(KiYi[1].go)
 		buffer= byteTransit(restoreO.parse, 1000000)
 
 
@@ -110,3 +132,4 @@ class YiOffCommand(sublime_plugin.TextCommand):
 		KiYi[0].stop()
 		KiYi[0]= None
 
+		KiYi[1].stop()
