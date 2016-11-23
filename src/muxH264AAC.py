42192add1608c0807594d4f6911a3495eceda5d8
NikolayRag <nikolayr@mail.ru>
2016-11-23 04:30:06 +0300

    MuxAAC got bool adts argment

--- src/muxH264AAC.py
+++ src/muxH264AAC.py
@@ -270,13 +270,18 @@ class MuxAAC():
 
 	sink= None
 
+	doADTS= True
 
-	def __init__(self, _sink):
+	def __init__(self, _sink, adts=True):
 		self.sink= _sink
 
 		if not self.sink:
 			kiLog.err('Sink not specified')
 			return
+
+
+		self.doADTS= adts
+
 
 
 	def add(self, _atom):
@@ -288,10 +293,35 @@ class MuxAAC():
 				kiLog.warn('Too big AAC found, skipped: %d' % len(_atom.data))
 				return
 
-			# -todo 99 (aac) +1: AAC header is reverse-engineered, ensure it is correct
-			aacPre= b'\xff\xf1\x4c\x80' +(len(_atom.data)*32+255).to_bytes(2,'big') +b'\xfc'
 
-			self.sink.add(aacPre +_atom.data)
+
+#  todo 108 (aac) +0: write (optional) ADTS header
+			'''
+A 	12 	syncword 0xFFF, all bits must be 1
+B 	1 	MPEG Version: 0 for MPEG-4, 1 for MPEG-2
+C 	2 	Layer: always 0
+D 	1 	protection absent, Warning, set to 1 if there is no CRC and 0 if there is CRC
+E 	2 	profile, the MPEG-4 Audio Object Type minus 1
+F 	4 	MPEG-4 Sampling Frequency Index (15 is forbidden)
+G 	1 	private bit, guaranteed never to be used by MPEG, set to 0 when encoding, ignore when decoding
+H 	3 	MPEG-4 Channel Configuration (in the case of 0, the channel configuration is sent via an inband PCE)
+I 	1 	originality, set to 0 when encoding, ignore when decoding
+J 	1 	home, set to 0 when encoding, ignore when decoding
+K 	1 	copyrighted id bit, the next bit of a centrally registered copyright identifier, set to 0 when encoding, ignore when decoding
+L 	1 	copyright id start, signals that this frame's copyright id bit is the first bit of the copyright id, set to 0 when encoding, ignore when decoding
+M 	13 	frame length, this value must include 7 or 9 bytes of header length: FrameLength = (ProtectionAbsent == 1 ? 7 : 9) + size(AACFrame)
+O 	11 	Buffer fullness
+P 	2 	Number of AAC frames (RDBs) in ADTS frame minus 1, for maximum compatibility always use 1 AAC frame per ADTS frame
+Q 	16 	CRC if protection absent is 0 
+			'''
+
+		
+
+			if self.doADTS:
+				adts= b'\xff\xf1\x4c\x80' +(len(_atom.data)*32+255).to_bytes(2,'big') +b'\xfc'
+				self.sink.add(adts +_atom.data)
+			else:
+				self.sink.add(_atom.data)
 
 
 	def stop(self):
