0d279efebedda1ad7e7a0831af1e7fd08baa991a
NikolayRag <nikolayr@mail.ru>
2016-12-22 22:19:57 +0300

    fix: allow AAC to start from mid-mp4

--- src/AACDetect.py
+++ src/AACDetect.py
@@ -1,4 +1,5 @@
 from .AACCore import *
+from .kiLog import *
 
 
 '''
@@ -6,9 +7,9 @@ Support class to detect Yi4k (only) AAC blocks looking pretty suitable
 It should be completely replaced by native AAC decoder. Eventually.
 '''
 class AACDetect():
-	#allowed max_sfb's
-	sfb8= 12
-	sfb1= [0,40]
+	#allowed max_sfb for [started]
+	sfb8= [[12], [12]]
+	sfb1= [[0,40], [40]]
 
 	started= False
 	seqNow= False
@@ -49,11 +50,13 @@ class AACDetect():
 			if (
 				aac.error
 				#Yi4k specific:
-				or (aac.sce_ics0.max_sfb!= (self.sfb8 if aac.sce_ics0.is8 else self.sfb1[self.started])) #predefined Maxsfb
+				or (aac.sce_ics0.max_sfb not in [self.sfb1,self.sfb8][aac.sce_ics0.is8][self.started]) #allowed Maxsfb
 				or (aac.sce_ics0.use_kb_window[0] == seqAfter)	#limit combinations
 			):
 				continue
 
+			if not self.started and not aac.sce_ics0.max_sfb:
+				kiLog.warn('AAC started from mid')
 
 			aacStartA.append(aacPos)
 
