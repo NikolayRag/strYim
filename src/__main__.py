6371af5e9ba0ded64f535e7f085259060fb100a2
NikolayRag <nikolayr@mail.ru>
2016-12-30 02:41:44 +0300

    add commandline entry point

---
+++ src/__main__.py
@@ -0,0 +1,26 @@
+import argparse, sys
+
+from stryim import *
+
+
+if __name__ == '__main__':
+	cParser= argparse.ArgumentParser(description= 'Yi 4k lossless streamer.', usage='''
+	%(prog)s -h|--help
+	%(prog)s destination
+Example:
+	%(prog)s rtmp://a.rtmp.youtube.com/live2/your-secret-key
+ ''')
+
+	cParser.add_argument('dst', metavar='destination', type=str, help='streaming destination; file or rtmp://')
+	cParser.add_argument('-logverb', metavar='class', type=str, nargs='+', help='Classes to log verb-level.')
+	cParser.add_argument('-logok', metavar='class', type=str, nargs='+', help='Classes to log ok-level.')
+	
+	try:
+		args= cParser.parse_args()
+	except:
+		sys.exit(0)
+
+
+	Stryim.start(args.dst)
+
+
