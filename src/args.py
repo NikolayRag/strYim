1c33dc91bcbbcc451d1c786c21850040a49ea632
NikolayRag <nikolayr@mail.ru>
2017-01-03 05:04:56 +0300

    clean commandline messages

--- src/args.py
+++ src/args.py
@@ -2,20 +2,14 @@ import argparse
 
 
 def parse(_gui=False):
-	cParser= argparse.ArgumentParser(description= 'Yi 4k lossless streamer.', usage='''
-	%(prog)s -h|--help
-	%(prog)s destination [-nonstop]
-Example:
-	%(prog)s rtmp://a.rtmp.youtube.com/live2/your-secret-key
-
--nonstop:
-	Dont exit when camera pauses.
- ''')
+	cParser= argparse.ArgumentParser(description= 'Yi 4k lossless streamer.')
 
 	cParser.add_argument('-nonstop', action='store_true', help='Dont exit when camera pauses.')
-	cParser.add_argument('-logverb', type=str, nargs='+', help='Classes to log verb-level.')
-	cParser.add_argument('-logwarn', type=str, nargs='+', help='Classes to log warn-level.')
-	cParser.add_argument('dst', type=str, nargs=('?' if _gui else None), help='streaming destination; rtmp://server/path')
+	cParser.add_argument('dst', type=str, nargs=('?' if _gui else None), help='streaming destination: rtmp://server/path')
+
+	cParser.add_argument('-logverb', type=str, nargs='+', help=argparse.SUPPRESS)
+	cParser.add_argument('-logwarn', type=str, nargs='+', help=argparse.SUPPRESS)
+
 	
 	try:
 		args= cParser.parse_args()
