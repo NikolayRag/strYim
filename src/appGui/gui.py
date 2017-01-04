a8f0fa48d4dd9d031cdb19972962a8a840c7f987
NikolayRag <nikolayr@mail.ru>
2017-01-05 02:19:48 +0300

    gui goes XML (.ui)

--- src/appGui/gui.py
+++ src/appGui/gui.py
@@ -1,23 +1,27 @@
-import os
+from os import path
 
 from PySide.QtCore import *
 from PySide.QtGui import *
-from PySide.QtDeclarative import QDeclarativeView
+from PySide.QtUiTools import *
+#from PySide.QtDeclarative import QDeclarativeView
 
 class gui():
 	QApp= None
 	view= None
 
-	modulePath= os.path.relpath(os.path.dirname(__file__))
+	modulePath= path.abspath(path.dirname(__file__))
 
 	def __init__(self):
 		self.QApp = QApplication('')
 		self.QApp.setStyle(QStyleFactory.create('plastique'))
 
-		self.view= QDeclarativeView()
-		self.view.setSource(QUrl( os.path.join(self.modulePath,'stryim.qml') ))
+		uiFile= path.join(self.modulePath,'stryim.ui')
+		self.view= QUiLoader().load(uiFile)
 		self.view.show()
 
+#		self.view= QDeclarativeView()
+#		self.view.setSource(QUrl( os.path.join(self.modulePath,'stryim.qml') ))
+#		self.view.show()
 
 	def exec(self):
 		self.QApp.exec_()
