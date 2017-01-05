4949c50c1428fd399af447a42dfa97ac2b7bbcce
NikolayRag <nikolayr@mail.ru>
2017-01-06 00:16:35 +0300

    add prototype main window (designed)

--- src/appGui/gui.py
+++ src/appGui/gui.py
@@ -17,11 +17,22 @@ class gui():
 
 		uiFile= path.join(self.modulePath,'stryim.ui')
 		self.view= QUiLoader().load(uiFile)
-		self.view.show()
+		self.view.setWindowFlags(Qt.FramelessWindowHint)
+
+		self.view.findChild(QWidget, "btnCamStop").hide()
+		self.view.findChild(QWidget, "radioCamAir").toggle()
+		self.view.findChild(QWidget, "radioCamReady").toggle()
+		self.view.findChild(QWidget, "radioCamIdle").toggle()
+		self.view.findChild(QWidget, "radioCamError").toggle()
+		self.view.findChild(QWidget, "radioCamNone").toggle()
+		
+
 
 #		self.view= QDeclarativeView()
 #		self.view.setSource(QUrl( os.path.join(self.modulePath,'stryim.qml') ))
 #		self.view.show()
 
 	def exec(self):
+		self.view.show()
+
 		self.QApp.exec_()
