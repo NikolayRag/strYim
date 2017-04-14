ff791a395f6e1dbce4d53ff681b459ff34cf6c5d
NikolayRag <nikolayr@mail.ru>
2017-04-14 03:38:04 +0300

    move comments

--- src/SourceYi4k/yiReader.py
+++ src/SourceYi4k/yiReader.py
@@ -4,6 +4,27 @@ from .yiReaderTelnet import *
 from .yiReaderAgent import *
 
 
+'''
+yiReader controls camera to provide h264+aac byte stream.
+
+yiReader flow:
+
+* send agent to camera
+* run agent at camera side
+* connect to agent
+	* recieve Atom data
+		* decode AAC
+
+'''
+
+#  todo 260 (YiAgent, check) +0: catch recording stops or cannot start
+#  todo 261 (YiAgent, check) +0: set camera settings
+
+# =todo 257 (feature, YiAgent) +0: Send agent to camera
+# =todo 258 (feature, YiAgent) +0: Run agent at camera
+# =todo 259 (feature, YiAgent) +0: Make camera agent
+
+
 class yiReader():
 	telnet= None
 
@@ -12,3 +33,10 @@ class yiReader():
 		
 		logging.info('Reader inited')
 
+
+
+
+
+	def test(self):
+		logging.info('test telnet')
+		
