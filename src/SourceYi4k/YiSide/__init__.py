0bb826a804c3339d58804f7e6ea8da864692c5cf
NikolayRag <nikolayr@mail.ru>
2017-05-05 15:12:50 +0300

    comment

--- src/SourceYi4k/YiSide/__init__.py
+++ src/SourceYi4k/YiSide/__init__.py
@@ -1,5 +1,8 @@
 '''
 Capture full-band stream from Yi4k camera.
+YiData(port) should be run at Yi4k side.
+Camera will wait for connection on port provided,
+and then stream data to it, when loop-record is on.
 '''
 
 from .YiData import *
