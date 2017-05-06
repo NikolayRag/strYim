b1009d8b5a8a3db0edbe160ef30a5b7149f976f2
NikolayRag <nikolayr@mail.ru>
2017-05-06 05:35:11 +0300

    test: add Yi4k case

--- src/test.py
+++ src/test.py
@@ -5,7 +5,7 @@ Yi4k camera required to be available at 192.168.42.1 and telnet-enabled
 
 import kiLog
 kiLog.state('YiReader', kiLog.DEBUG)
-kiLog.state('YiPy', kiLog.DEBUG)
+kiLog.state('Yi4k', kiLog.DEBUG)
 kiLog.state('', kiLog.INFO)
 
 
@@ -81,11 +81,39 @@ def t3():
 	logging.info('YiReader: %s, %d rate' % (yiReaderRes, dataLen[0]/(time.time()-t1[0])))
 
 
-t1()
-print()
-t2()
-print()
-t3()
-print()
+
+
+def t4():
+	def atomCB(atom):
+		aType='?'
+		if atom.typeMoov:
+			aType= 'MOOV'
+		if atom.typeAVC:
+			aType= '264'
+			if atom.AVCKey:
+				aType+= ' key'
+			if not atom.AVCVisible:
+				aType+= ' invis'
+		if atom.typeAAC:
+			aType= 'AAC'
+
+
+		print(aType, atom.outPos-atom.inPos)
+
+	yi4k= SourceYi4k.Yi4k(atomCB)
+	threading.Timer(10, yi4k.stop).start()
+	yi4k.start()
+#	yiReaderRes= yi4k.wait()
+
+
+#	logging.info('Yi4k: %s' % yiReaderRes)
+
+
+
+
+#t1(); print()
+#t2(); print()
+#t3(); print()
+t4(); print()
 
 logging.info('End')
