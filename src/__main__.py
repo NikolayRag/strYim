d17ba768c34aa95990184ba824cfbb71835e9840
NikolayRag <nikolayr@mail.ru>
2017-05-22 00:12:49 +0300

    cleanup console app flow

--- src/__main__.py
+++ src/__main__.py
@@ -1,9 +1,4 @@
-import logging
-import kiLog
-kiLog.state(False)
-kiLog.state('', kiLog.INFO)
-kiLog.state('Streamer', kiLog.INFO)
-kiLog.state('Yi4k', kiLog.INFO)
+import logging, kiLog
 
 
 from args import *
@@ -13,64 +8,46 @@ from SourceYi4k import *
 import threading
 
 
-appSource= None
-appStreamer= None
-
-
-
-
-
 '''
-Commandline flow.
-Streaming is done once using some settings,
-till interrupted or camera stops (if not -nonstop).
+Streaming is done once till interrupted or camera stops.
 '''
-def runCmdline(_args):
-	#Check for ability to run
-#	cFormat= _args.formats[0]
-#	logging.info('Setting ' +str(cFormat['yi']))
-#	if not flowCamControl.start(cFormat['yi']):
-#		logging.error('Camera error')
-#		return
-
-
-	#init
-	appSource= Yi4k()
-	appStreamer= Streamer(_args.args['dst'])
-	appStreamer.link(appSource)
-
-	appSource.start()
-
-
-	while not appSource.isIdle():
-		try:
-			time.sleep(.1)
-		except KeyboardInterrupt:
-			logging.info('Terminated')
-			
-#			_args.args['nonstop']= True
-			break
-
-	threading.Timer(0,appSource.stop).start()
-	threading.Timer(0,appStreamer.close).start()
-
-
-
-
-
-
 if __name__ == '__main__':
 	cArgs= Args(False)
 
 	if cArgs.args:
+		kiLog.state(False)
+		kiLog.state('', kiLog.INFO)
+		kiLog.state('Streamer', kiLog.INFO)
+		kiLog.state('Yi4k', kiLog.INFO)
+		
 		for c in (cArgs.args['logverb'] or []):
 			kiLog.state(c, kiLog.DEBUG)
 		for c in (cArgs.args['logwarn'] or []):
 			kiLog.state(c, kiLog.WARING)
 
-		runCmdline(cArgs)
+
+		#init
+		appSource= Yi4k()
+		appStreamer= Streamer(cArgs.args['dst'])
+		appStreamer.link(appSource)
+
+		appSource.start()
 
 
+		#handle ctrl-c
+		while not appSource.isIdle():
+			try:
+				time.sleep(.1)
+			except KeyboardInterrupt:
+				logging.info('Terminated')
+				break
+
+		#finish
+		threading.Timer(0,appSource.stop).start()
+		threading.Timer(0,appStreamer.close).start()
+
+
+		#prevent breaking shutdows routines by ctrl-c
 		while len(threading.enumerate())>1:
 			try:
 				time.sleep(.1)
